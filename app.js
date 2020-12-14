eb0853c16325aa50e35bb49add4d07ab5f356d9a
Tay <tayk.dev@gmail.com>
2020-12-13 22:05:45 -0800

    Add files via upload
    JUST IN CASE I hadn't updated GitHub

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const button = document.getElementById("button");
-const body = document.getElementById("body");
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const red = document.getElementById("red");
+const orange = document.getElementById("orange");
+const yellow = document.getElementById("yellow");
+const green = document.getElementById("green");
+const blue = document.getElementById("blue");
+const purple = document.getElementById("purple");
 
-body.style.backgroundColor = 'red'
+const body = document.getElementById("body");
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+
+body.style.backgroundColor = 'white'
 button.addEventListener('click', changeBackground)
 
-function changeBackground(){
-    const colorIndex = parseInt(Math.random()*colors.length)
-    body.style.backgroundColor = colors[colorIndex]
+function changeBackground(color){
+    body.style.backgroundColor = color
 }
